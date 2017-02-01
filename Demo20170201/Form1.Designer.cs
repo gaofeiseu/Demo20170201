61cd7b98b52c86003c9274c3648ba2cab626d616
gaofeiseu <gaofeiseu@sina.com>
2017-02-01 22:22:40 +0800

    增加了导航按钮栏

--- Demo20170201/Form1.Designer.cs
+++ Demo20170201/Form1.Designer.cs
@@ -34,15 +34,28 @@
             this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
             this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
             this.toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
             this.toolStripDropDownButton2 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.联系我们ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.提建议ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.常见问题ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripDropDownButton3 = new System.Windows.Forms.ToolStripDropDownButton();
             this.toolStripDropDownButton4 = new System.Windows.Forms.ToolStripDropDownButton();
-            this.常见问题ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.提建议ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.联系我们ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
             this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.button_首页 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button_采购 = new System.Windows.Forms.Button();
+            this.button_销售 = new System.Windows.Forms.Button();
+            this.button_出入库 = new System.Windows.Forms.Button();
+            this.button_库存 = new System.Windows.Forms.Button();
+            this.button_客户 = new System.Windows.Forms.Button();
+            this.button_供应商 = new System.Windows.Forms.Button();
+            this.button_报表 = new System.Windows.Forms.Button();
+            this.button_系统 = new System.Windows.Forms.Button();
             this.statusStrip1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // statusStrip1
@@ -82,6 +95,17 @@
             this.toolStripStatusLabel3.Spring = true;
             this.toolStripStatusLabel3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
+            // toolStripDropDownButton1
+            // 
+            this.toolStripDropDownButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton1.Image")));
+            this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+            this.toolStripDropDownButton1.Padding = new System.Windows.Forms.Padding(2, 2, 0, 0);
+            this.toolStripDropDownButton1.ShowDropDownArrow = false;
+            this.toolStripDropDownButton1.Size = new System.Drawing.Size(54, 24);
+            this.toolStripDropDownButton1.Tag = "锁屏";
+            this.toolStripDropDownButton1.Text = "锁屏";
+            // 
             // toolStripDropDownButton2
             // 
             this.toolStripDropDownButton2.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
@@ -94,6 +118,24 @@
             this.toolStripDropDownButton2.Padding = new System.Windows.Forms.Padding(2, 2, 0, 0);
             this.toolStripDropDownButton2.Size = new System.Drawing.Size(63, 24);
             this.toolStripDropDownButton2.Text = "帮助";
+            // 
+            // 联系我们ToolStripMenuItem
+            // 
+            this.联系我们ToolStripMenuItem.Name = "联系我们ToolStripMenuItem";
+            this.联系我们ToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.联系我们ToolStripMenuItem.Text = "联系我们";
+            // 
+            // 提建议ToolStripMenuItem
+            // 
+            this.提建议ToolStripMenuItem.Name = "提建议ToolStripMenuItem";
+            this.提建议ToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.提建议ToolStripMenuItem.Text = "提建议";
+            // 
+            // 常见问题ToolStripMenuItem
+            // 
+            this.常见问题ToolStripMenuItem.Name = "常见问题ToolStripMenuItem";
+            this.常见问题ToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.常见问题ToolStripMenuItem.Text = "常见问题";
             // 
             // toolStripDropDownButton3
             // 
@@ -115,35 +157,6 @@
             this.toolStripDropDownButton4.Size = new System.Drawing.Size(78, 24);
             this.toolStripDropDownButton4.Text = "在线客服";
             // 
-            // 常见问题ToolStripMenuItem
-            // 
-            this.常见问题ToolStripMenuItem.Name = "常见问题ToolStripMenuItem";
-            this.常见问题ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
-            this.常见问题ToolStripMenuItem.Text = "常见问题";
-            // 
-            // 提建议ToolStripMenuItem
-            // 
-            this.提建议ToolStripMenuItem.Name = "提建议ToolStripMenuItem";
-            this.提建议ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
-            this.提建议ToolStripMenuItem.Text = "提建议";
-            // 
-            // 联系我们ToolStripMenuItem
-            // 
-            this.联系我们ToolStripMenuItem.Name = "联系我们ToolStripMenuItem";
-            this.联系我们ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
-            this.联系我们ToolStripMenuItem.Text = "联系我们";
-            // 
-            // toolStripDropDownButton1
-            // 
-            this.toolStripDropDownButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton1.Image")));
-            this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
-            this.toolStripDropDownButton1.Padding = new System.Windows.Forms.Padding(2, 2, 0, 0);
-            this.toolStripDropDownButton1.ShowDropDownArrow = false;
-            this.toolStripDropDownButton1.Size = new System.Drawing.Size(54, 24);
-            this.toolStripDropDownButton1.Tag = "锁屏";
-            this.toolStripDropDownButton1.Text = "锁屏";
-            // 
             // notifyIcon1
             // 
             this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
@@ -151,11 +164,131 @@
             this.notifyIcon1.Text = "notifyIcon1";
             this.notifyIcon1.Visible = true;
             // 
+            // button_首页
+            // 
+            this.button_首页.Image = ((System.Drawing.Image)(resources.GetObject("button_首页.Image")));
+            this.button_首页.Location = new System.Drawing.Point(300, 0);
+            this.button_首页.Margin = new System.Windows.Forms.Padding(0);
+            this.button_首页.Name = "button_首页";
+            this.button_首页.Size = new System.Drawing.Size(77, 100);
+            this.button_首页.TabIndex = 1;
+            this.button_首页.UseVisualStyleBackColor = true;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.pictureBox1);
+            this.flowLayoutPanel1.Controls.Add(this.button_首页);
+            this.flowLayoutPanel1.Controls.Add(this.button_采购);
+            this.flowLayoutPanel1.Controls.Add(this.button_销售);
+            this.flowLayoutPanel1.Controls.Add(this.button_出入库);
+            this.flowLayoutPanel1.Controls.Add(this.button_库存);
+            this.flowLayoutPanel1.Controls.Add(this.button_客户);
+            this.flowLayoutPanel1.Controls.Add(this.button_供应商);
+            this.flowLayoutPanel1.Controls.Add(this.button_报表);
+            this.flowLayoutPanel1.Controls.Add(this.button_系统);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1384, 100);
+            this.flowLayoutPanel1.TabIndex = 200;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 100);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button_采购
+            // 
+            this.button_采购.Image = ((System.Drawing.Image)(resources.GetObject("button_采购.Image")));
+            this.button_采购.Location = new System.Drawing.Point(377, 0);
+            this.button_采购.Margin = new System.Windows.Forms.Padding(0);
+            this.button_采购.Name = "button_采购";
+            this.button_采购.Size = new System.Drawing.Size(77, 100);
+            this.button_采购.TabIndex = 2;
+            this.button_采购.UseVisualStyleBackColor = true;
+            // 
+            // button_销售
+            // 
+            this.button_销售.Image = ((System.Drawing.Image)(resources.GetObject("button_销售.Image")));
+            this.button_销售.Location = new System.Drawing.Point(454, 0);
+            this.button_销售.Margin = new System.Windows.Forms.Padding(0);
+            this.button_销售.Name = "button_销售";
+            this.button_销售.Size = new System.Drawing.Size(77, 100);
+            this.button_销售.TabIndex = 3;
+            this.button_销售.UseVisualStyleBackColor = true;
+            // 
+            // button_出入库
+            // 
+            this.button_出入库.Image = ((System.Drawing.Image)(resources.GetObject("button_出入库.Image")));
+            this.button_出入库.Location = new System.Drawing.Point(531, 0);
+            this.button_出入库.Margin = new System.Windows.Forms.Padding(0);
+            this.button_出入库.Name = "button_出入库";
+            this.button_出入库.Size = new System.Drawing.Size(77, 100);
+            this.button_出入库.TabIndex = 4;
+            this.button_出入库.UseVisualStyleBackColor = true;
+            // 
+            // button_库存
+            // 
+            this.button_库存.Image = ((System.Drawing.Image)(resources.GetObject("button_库存.Image")));
+            this.button_库存.Location = new System.Drawing.Point(608, 0);
+            this.button_库存.Margin = new System.Windows.Forms.Padding(0);
+            this.button_库存.Name = "button_库存";
+            this.button_库存.Size = new System.Drawing.Size(77, 100);
+            this.button_库存.TabIndex = 5;
+            this.button_库存.UseVisualStyleBackColor = true;
+            // 
+            // button_客户
+            // 
+            this.button_客户.Image = ((System.Drawing.Image)(resources.GetObject("button_客户.Image")));
+            this.button_客户.Location = new System.Drawing.Point(685, 0);
+            this.button_客户.Margin = new System.Windows.Forms.Padding(0);
+            this.button_客户.Name = "button_客户";
+            this.button_客户.Size = new System.Drawing.Size(77, 100);
+            this.button_客户.TabIndex = 6;
+            this.button_客户.UseVisualStyleBackColor = true;
+            // 
+            // button_供应商
+            // 
+            this.button_供应商.Image = ((System.Drawing.Image)(resources.GetObject("button_供应商.Image")));
+            this.button_供应商.Location = new System.Drawing.Point(762, 0);
+            this.button_供应商.Margin = new System.Windows.Forms.Padding(0);
+            this.button_供应商.Name = "button_供应商";
+            this.button_供应商.Size = new System.Drawing.Size(77, 100);
+            this.button_供应商.TabIndex = 7;
+            this.button_供应商.UseVisualStyleBackColor = true;
+            // 
+            // button_报表
+            // 
+            this.button_报表.Image = ((System.Drawing.Image)(resources.GetObject("button_报表.Image")));
+            this.button_报表.Location = new System.Drawing.Point(839, 0);
+            this.button_报表.Margin = new System.Windows.Forms.Padding(0);
+            this.button_报表.Name = "button_报表";
+            this.button_报表.Size = new System.Drawing.Size(77, 100);
+            this.button_报表.TabIndex = 8;
+            this.button_报表.UseVisualStyleBackColor = true;
+            // 
+            // button_系统
+            // 
+            this.button_系统.Image = ((System.Drawing.Image)(resources.GetObject("button_系统.Image")));
+            this.button_系统.Location = new System.Drawing.Point(916, 0);
+            this.button_系统.Margin = new System.Windows.Forms.Padding(0);
+            this.button_系统.Name = "button_系统";
+            this.button_系统.Size = new System.Drawing.Size(77, 100);
+            this.button_系统.TabIndex = 9;
+            this.button_系统.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1384, 862);
+            this.Controls.Add(this.flowLayoutPanel1);
             this.Controls.Add(this.statusStrip1);
             this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -166,6 +299,8 @@
             this.Text = "中国电信精品渠道终端销售系统";
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -185,6 +320,17 @@
         private System.Windows.Forms.ToolStripMenuItem 常见问题ToolStripMenuItem;
         private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
         private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Button button_首页;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button_采购;
+        private System.Windows.Forms.Button button_销售;
+        private System.Windows.Forms.Button button_出入库;
+        private System.Windows.Forms.Button button_库存;
+        private System.Windows.Forms.Button button_客户;
+        private System.Windows.Forms.Button button_供应商;
+        private System.Windows.Forms.Button button_报表;
+        private System.Windows.Forms.Button button_系统;
     }
 }
 
